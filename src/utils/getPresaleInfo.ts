839f3f48202d29bca9c75870bfc3b58be3f29c79
Muhammad Arslan <arslan@areax.ai>
2024-11-16 11:46:03 +0500

    feat: flow completed

---
+++ src/utils/getPresaleInfo.ts
@@ -0,0 +1,27 @@
+import { connection, idlFile, programID } from "@/constants";
+import { AnchorWallet } from "@solana/wallet-adapter-react";
+import { getPresalePDA, getUserInfoPDA } from "./helpers";
+import * as anchor from "@project-serum/anchor";
+
+export const getPresaleInfo = async (wallet: AnchorWallet) => {
+  const provider = new anchor.AnchorProvider(connection, wallet, {});
+  const program = new anchor.Program(idlFile, programID, provider);
+  const [presalePDA] = await getPresalePDA();
+  const userInfo = await getUserInfoPDA(wallet);
+
+  console.log(presalePDA.toBase58(), userInfo.toBase58(), program);
+  try {
+    const transaction = await program.account.presaleInfo.fetch(presalePDA)
+    const user = await program.account.userInfo.fetch(userInfo)
+      // .rpc();
+      const decimals = 9; // Example: Token with 9 decimals
+
+// Convert to float using string manipulation
+const floatValue = parseFloat(user.buyTokenAmount.toNumber());
+
+console.log("Floating Value:", floatValue);
+    console.log({user});
+  } catch (error: any | unknown) {
+    console.error("could not log data: ", error.message);
+  }
+};
